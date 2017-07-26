0d54f081e310c3fea9c3b3e833bb49ff12c28c6c
linzme <805087754@qq.com>
2017-07-26 21:11:02 +0800

    Signed-off-by: linzme <805087754@qq.com>

---
+++ js/common-topbar.js
@@ -0,0 +1,130 @@
+function overlink1(){
+	var a = document.getElementById('common-topbar-menu2-link1');
+	a.style.display = "block";
+}
+function outlink1(){
+	var a = document.getElementById('common-topbar-menu2-link1');
+	a.style.display = "none";
+}
+
+function overlink2(){
+	var a = document.getElementById('common-topbar-menu2-link2');
+	a.style.display = "block";
+}
+function outlink2(){
+	var a = document.getElementById('common-topbar-menu2-link2');
+	a.style.display = "none";
+}
+function overlink3(){
+	var a = document.getElementById('common-topbar-menu2-link3');
+	a.style.display = "block";
+}
+function outlink3(){
+	var a = document.getElementById('common-topbar-menu2-link3');
+	a.style.display = "none";
+}
+function overlink4(){
+	var a = document.getElementById('common-topbar-menu2-link4');
+	a.style.display = "block";
+}
+function outlink4(){
+	var a = document.getElementById('common-topbar-menu2-link4');
+	a.style.display = "none";
+}
+function overlink5(){
+	var a = document.getElementById('common-topbar-menu2-link5');
+	a.style.display = "block";
+}
+function outlink5(){
+	var a = document.getElementById('common-topbar-menu2-link5');
+	a.style.display = "none";
+}
+function overlink6(){
+	var a = document.getElementById('common-topbar-menu2-link6');
+	a.style.display = "block";
+}
+function outlink6(){
+	var a = document.getElementById('common-topbar-menu2-link6');
+	a.style.display = "none";
+}
+
+function transfrom(){
+	var a = document.getElementById('content-warp2-middonid');
+	a.style.transform = "rotateY(180deg)";
+	a.style.transition = "transform 1.5s";
+}
+function transfromout(){
+	var a = document.getElementById('content-warp2-middonid');
+	a.style.transform = "rotateY(360deg)";
+	a.style.transition = "transform 1.5s";
+}
+function margintop1(){
+	var a = document.getElementById('right3');
+	var b = document.getElementById('right2');
+	var c = document.getElementById('right1');
+	b.style.marginTop = "5px";
+	a.style.marginTop = "10px";
+//	c.style.transition = "width 0.5s,height 0.5s";
+}
+function marginout1(){
+	var a = document.getElementById('right2');
+	var b = document.getElementById('right3');
+	var c = document.getElementById('right1');
+//	c.style.transition = "width 0.5s,height 0.5s";
+	a.style.marginTop = "10px";
+	b.style.marginTop = "10px";
+}
+function margintop2(){
+	var a = document.getElementById('right3');
+	var b = document.getElementById('right2');
+	b.style.marginTop = "5px";
+	a.style.marginTop = "5px";
+}
+function marginout2(){
+	var a = document.getElementById('right2');
+	var b = document.getElementById('right3');
+	a.style.marginTop = "10px";
+	b.style.marginTop = "10px";
+}
+function margintop3(){
+	var a = document.getElementById('right3');
+	a.style.marginTop = "5px";
+}
+function marginout3(){
+	var b = document.getElementById('right3');
+	b.style.marginTop = "10px";
+}
+function overcontent(){
+	var b = document.getElementById('left1-content3');
+	var c = document.getElementById('left1-content4');
+	var d = document.getElementById('left1-content5');
+	var e = document.getElementById('left1-content6');
+	var f = document.getElementById('left1-content7');
+	b.style.width = "150px";
+	b.style.transition = "width 2s";
+	c.style.width = "200px";
+	c.style.transition = "width 2s";
+	d.style.width = "180px";
+	d.style.transition = "width 2s";
+	e.style.width = "240px";
+	e.style.transition = "width 2s";
+	f.style.width = "270px";
+	f.style.transition = "width 2s";
+}
+function outcontent(){
+	var a = document.getElementById('left1-content3');
+	var b = document.getElementById('left1-content4');
+	var c = document.getElementById('left1-content5');
+	var d = document.getElementById('left1-content6');
+	var e = document.getElementById('left1-content7');
+	a.style.width = "50px";
+	a.style.transition = "width 2s";
+	b.style.width = "280px";
+	b.style.transition = "width 2s";
+	c.style.width = "130px";
+	c.style.transition = "width 2s";
+	d.style.width = "200px";
+	d.style.transition = "width 2s";
+	e.style.width = "300px";
+	e.style.transition = "width 2s";
+}
